eaae6b4a50e110b4fe79bf414e0b66a55a0d42e1
Alan Guerrero <alan.guerrero@gmail.com>
2016-05-31 19:14:33 -0400

    Replacing App component by the Route component, and providing routing libraries.

--- React_Redux/blog/src/index.js
+++ React_Redux/blog/src/index.js
@@ -3,13 +3,18 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
+// broweserHistory: whenever the URL updates, react-router is going to interpret everything after the browse protocol (the root URL).
+import { Router, browserHistory } from 'react-router';
+
 import reducers from './reducers';
+
+// importing Routes
+import routes from './routes';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <Router history={browserHistory} routes={routes}/>
   </Provider>
   , document.querySelector('.container'));
