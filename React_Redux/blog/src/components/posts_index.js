47f6ffcbe4310e04c2b60bfa938bb3d29aade96d
Alan Guerrero <alan.guerrero@gmail.com>
2016-06-05 03:17:45 -0400

    Adding a Link via react router to the new posts component

--- React_Redux/blog/src/components/posts_index.js
+++ React_Redux/blog/src/components/posts_index.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchPosts } from '../actions/index';
 
+// Link: A React component, which is an anchor tag.
+import { Link } from 'react-router';
+
 class PostsIndex extends Component {
   // A Lifecycle method: automatically called by React when the component is going to be rendered by the DOM for the first time.
   componentWillMount(){
@@ -11,7 +14,13 @@ class PostsIndex extends Component {
 
   render() {
     return (
-      <div>List of blogs</div>
+      <div>
+        <div className="text-xs-right">
+          <Link to="/posts/new" className="btn btn-primary">
+            Add a Post
+          </Link>
+        </div>
+      </div>
     );
   }
 }
