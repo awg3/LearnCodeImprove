dd01d0f337e13c0b6c15c9a5e8fe25a506306539
Alan Guerrero <alan.guerrero@gmail.com>
2016-06-05 03:16:03 -0400

    Adding the create post action

--- React_Redux/blog/src/actions/index.js
+++ React_Redux/blog/src/actions/index.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
 export const FETCH_POSTS = "FETCH_POSTS";
+export const CREATE_POST = "CREATE_POST";
+
 const ROOT_URL = "http://reduxblog.herokuapp.com/api";
 const API_KEY = "?key=bac123everybodyyou&me";
 
@@ -11,4 +13,14 @@ export function fetchPosts() {
     type: FETCH_POSTS,
     payload: request
   };
+}
+
+export function createPost(props) {
+  // props contains an object which has the title, categories and content of the post.
+  const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, props);
+
+  return {
+    type: CREATE_POST,
+    payload: request
+  }
 }
