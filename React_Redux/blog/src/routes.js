15024cbb37e885b1565620e4e03cdde286a95e8d
Alan Guerrero <alan.guerrero@gmail.com>
2016-06-09 15:14:26 -0400

    Adding new route based on post id

--- React_Redux/blog/src/routes.js
+++ React_Redux/blog/src/routes.js
@@ -4,10 +4,17 @@ import { Route, IndexRoute } from 'react-router';
 import App from './components/app';
 import PostsIndex from './components/posts_index';
 import PostsNew from './components/posts_new';
+import PostsShow from './components/posts_show';
 
 export default (
   <Route path='/' component={App}>
     <IndexRoute component={PostsIndex} />
     <Route path="posts/new" component={PostsNew} />
+    <Route path="posts/:id" component={PostsShow} />
   </Route>
 );
+
+/*
+  The route path for PostsShow is populated via :id.
+  :id is parsed by ReactJS by using the provided id from this.props.params.id
+*/
