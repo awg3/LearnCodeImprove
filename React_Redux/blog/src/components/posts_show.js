78bcd2782f7e5f20165b01c37625e34433672294
Alan Guerrero <alan.guerrero@gmail.com>
2016-06-10 16:00:35 -0400

    Added routing and even handling for the delete post action

--- React_Redux/blog/src/components/posts_show.js
+++ React_Redux/blog/src/components/posts_show.js
@@ -1,20 +1,68 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { fetchPost } from '../actions/index';
+import { deletePost } from '../actions/index';
+import { Link } from 'react-router';
 
 class PostsShow extends Component {
+
+  // contextTypes: gives PostsShow access to the router.
+  static contextTypes = {
+    router: PropTypes.object
+  };
+
   componentWillMount(){
     // This method ensures that anything within will run as soon as the component is about to show.
     // fetching the post via id.
     this.props.fetchPost(this.props.params.id);
   }
+
+  onDeleteClick(){
+    this.props.deletePost(this.props.params.id).then(() => {
+      this.context.router.push('/');
+    });
+  }
+
   render(){
-    return (
-      <div>
-        Show post {this.props.params.id}
-      </div>
-    );
+    // ES6: const props = this.props.post
+    const { post } = this.props;
+
+    if(!post){
+      // Load this while the promise resolves to get the post.
+      return(
+        <div>Loading...</div>
+      );
+    }
+    else {
+      return (
+        <div>
+          <div className="row">
+            <div className="col-xs-6">
+              <Link to="/" className="btn btn-primary pull-xs-left">
+                Back
+              </Link>
+            </div>
+            <div className="col-xs-6">
+              <button className="btn btn-danger pull-xs-right" onClick={this.onDeleteClick.bind(this)}>
+                Delete Post
+              </button>
+            </div>
+          </div>
+          <div className="margin__top-bottom-30px">
+            <h3>Title: { post.title }</h3>
+            <h6>Categories: { post.categories }</h6>
+            <p>Content: { post.content }</p>
+          </div>
+        </div>
+      );
+    }
   }
 }
 
-export default connect(null, { fetchPost })(PostsShow);
+function mapStateToProps(state){
+  return {
+    post: state.posts.post
+  };
+}
+
+export default connect(mapStateToProps, { fetchPost, deletePost })(PostsShow);
